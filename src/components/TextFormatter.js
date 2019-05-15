eb4f61ab03786885fc2a259e499ead186a6ccdb0
Cara Ponzini <ponzini@gmail.com>
2019-05-15 14:12:15 -0700

    changes font too

--- src/components/TextFormatter.js
+++ src/components/TextFormatter.js
@@ -3,7 +3,8 @@ import React, { PureComponent } from 'react';
 export default class TextFormatter extends PureComponent {
   state = {
     text: 'type here',
-    color: 'black'
+    color: 'black',
+    font:'Arial'
   }
 
   handleSubmit = event => {
@@ -18,29 +19,35 @@ export default class TextFormatter extends PureComponent {
     this.setState({ color: target.value });
   }
 
+  handleFontChange = ({ target }) => {
+    this.setState({ font: target.value });
+  }
+
   render() {
     const {
       text,
-      color
+      color,
+      font
     } = this.state;
 
     return (
       <>
       <form onSubmit={this.handleSubmit}>
+        <select name="font" onChange={this.handleFontChange}>
+          <option value={'Arial, Helvetica, sans-serif'}>Arial</option>
+          <option value={'Times New Roman, Times, serif'}>Times</option>
+          <option value={'Courier New, Courier, monospace'}>Courier</option>
+        </select>
         <input type="color" onChange={this.handleColorChange}/>
         <input name="text" value={text} onChange={this.handleTextChange} type="text" />
         <button>Enter</button>
       </form>
       <div>
-        <p style={{ color: color }}>{text}</p>
+        <p style={{ color: color, fontFamily: font }}>{text}</p>
       </div>
       </>
     );
   }
 }
 
-{/* <select onChange={this.handleColorChange}>
-<option value={'black'}>black</option>
-<option value={'blue'}>blue</option>
-<option value={'red'}>red</option>
-</select> */}
+
