5511921c60bbd722228ade269e3ad5862c19c19f
Cara Ponzini <ponzini@gmail.com>
2019-05-15 16:11:31 -0700

    spacing in rgb

--- src/components/Color.js
+++ src/components/Color.js
@@ -5,7 +5,7 @@ function hexToRGB(hex) {
   const r = parseInt(hex.slice(1, 3), 16);
   const g = parseInt(hex.slice(3, 5), 16);
   const b = parseInt(hex.slice(5), 16);
-  return (`${r},${g},${b}`);
+  return (`${r}, ${g}, ${b}`);
 }
 
 function Color({ name, hex, rgb }) {
